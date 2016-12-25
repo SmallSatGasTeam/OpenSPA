f545f5044e310a29c7b267978d668e2d050b15b1
Nick Benoit <nick.benoit14@gmail.com>
2016-12-25 03:33:09 +0000

    adds test fixture

--- test/local_communicator_test.hpp
+++ test/local_communicator_test.hpp
@@ -3,6 +3,26 @@
 #include <local_communicator.hpp>
 #include <routing_table.hpp>
 
+class LocalCommunicatorTest : public ::testing::Test {
+public:
+  virtual void SetUp(){
+    opcode = 0;
+    port = 9;
+    addr = std::make_shared<LogicalAddress>(1,1);
+    fullTable->insert(*addr, port);
+    message = std::make_shared<SpaMessage>(*addr, 0);
+  }
+
+
+  std::shared_ptr<RoutingTable> fullTable;
+  std::shared_ptr<RoutingTable> emptyTable;
+  std::shared_ptr<LogicalAddress> addr;
+  std::shared_ptr<SpaMessage> message;
+  uint16_t port;
+  uint8_t opcode;
+};
+
+
 TEST(LocalCommunicator, send__address_exists_in_table){
   std::shared_ptr<MockServerSocket> sock = std::make_shared<MockServerSocket>();
   std::shared_ptr<RoutingTable> rt = std::make_shared<RoutingTable>();
@@ -21,5 +41,7 @@ TEST(LocalCommunicator, send__address_exists_in_table){
   lc.send(message);
 }
 
-// TEST(LocalCommunicator, send__not_in_table){}
+TEST(LocalCommunicator, send__not_in_table){
+
+}
 // TEST(LocalCommunicator, listen){}

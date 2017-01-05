64e9fd9596af76c2ce7cd1217e7bbc66d102ae79
Jack Conrad Kiefer II <jack.c.kiefer@gmail.com>
2017-01-05 09:54:28 -0700

    initial commit (#20)

--- test/mocks/platform_abstraction/socket/server_socket.hpp
+++ test/mocks/platform_abstraction/socket/server_socket.hpp
@@ -3,4 +3,5 @@ class MockServerSocket : public ServerSocket
 {
 public:
   MOCK_METHOD4(send, bool(std::string, uint16_t, uint8_t *, uint32_t));
+  MOCK_METHOD1(listen, void(ServerSocket::MessageCallback));
 };

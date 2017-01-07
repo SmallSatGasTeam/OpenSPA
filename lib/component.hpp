007319fe3f06a4ec0648329d9efeadc87908eb3d
Nick Benoit <nick.benoit14@gmail.com>
2017-01-07 17:15:00 +0000

    component compiles

--- lib/component.hpp
+++ lib/component.hpp
@@ -1,22 +1,23 @@
 #ifndef COMPONENT_HPP
 #define COMPONENT_HPP
 
-#include "application_communicator.hpp"
+#include "spa_communicator.hpp"
 #include <memory>
 
 class Component{
 public:
-  typedef std::shared_ptr<ApplicationCommunicator> Com;
+  typedef std::shared_ptr<SpaCommunicator> Com;
 
   Component(Com communicator = nullptr):communicator(communicator){}
-  virtual void appInit();
+  // virtual ~Component(){}
+  virtual void appInit() = 0;
   // virtual void appShutdown();
   // virtual int32_t publish();
   // virtual void registerRequest();
   virtual void sendMsg(std::shared_ptr<SpaMessage> message);
   // virtual void receiveMessage(SpaMessage);
 protected:
-  LogicalAddress address;
+  // LogicalAddress address;
   Com communicator;
 };
 #endif

f9c9da896061d62e6142cffef8987d6a299d941e
Nick Benoit <nick.benoit14@gmail.com>
2017-01-07 16:40:10 +0000

    adds communicator

---
+++ lib/component.cpp
@@ -0,0 +1,6 @@
+#include "component.hpp"
+
+void Component::sendMsg(std::shared_ptr<SpaMessage> message){
+  if(message == nullptr || communicator == nullptr){return;}
+  communicator->send(message);
+}
--- lib/component.hpp
+++ lib/component.hpp
@@ -1,11 +1,22 @@
 #ifndef COMPONENT_HPP
 #define COMPONENT_HPP
+
+#include "application_communicator.hpp"
+#include <memory>
+
 class Component{
-  // virtual void appInit();
+public:
+  typedef std::shared_ptr<ApplicationCommunicator> Com;
+
+  Component(Com communicator = nullptr):communicator(communicator){}
+  virtual void appInit();
   // virtual void appShutdown();
   // virtual int32_t publish();
   // virtual void registerRequest();
-  // virtual void sendMsg();
+  virtual void sendMsg(std::shared_ptr<SpaMessage> message);
   // virtual void receiveMessage(SpaMessage);
+protected:
+  LogicalAddress address;
+  Com communicator;
 };
 #endif

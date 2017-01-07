2390d8d02a42352644dc467fc05ecd2f90bd8b85
Nick Benoit <nick.benoit14@gmail.com>
2017-01-07 17:38:13 +0000

    create message in SpaCommuncator callback

--- lib/subnet_manager.cpp
+++ lib/subnet_manager.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <subnet_manager.hpp>
+#include <spa_message.hpp>
 
 
 void SubnetManager::messageCallback(uint8_t* buff, uint32_t len){
-  for(int i = 0; i < len; i++){
-    std::cout << buff[i];
-  }
-  std::cout << '\n';
+  auto message = SpaMessage::unmarshal(buff, len);
+  std::cout << "Opcode: " <<(int)message->opcode << '\n';
   return;
 }

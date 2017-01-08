6236ed8499afabbd8b6d31dfc96743544859c898
Jack Conrad Kiefer II <jack.c.kiefer@gmail.com>
2017-01-07 17:40:42 -0700

    Physical instead of Local comms / add selectCommunicator to test fixture (#24)
    * testing physical com instead of local
    
    * add selectCommunicator to test fixture

--- test/spa_communicator_test.hpp
+++ test/spa_communicator_test.hpp
@@ -1,6 +1,6 @@
 #include <spa_communicator.hpp>
 
-#include "mocks/local_communicator.hpp"
+#include "mocks/physical_communicator.hpp"
 
 class SpaCommunicatorTest : public ::testing::Test
 {
@@ -8,17 +8,17 @@ public:
   virtual void SetUp()
   {
     comms.push_back(
-        localCom = std::make_shared<MockLocalCommunicator>(LogicalAddress(1, 0)));
+        localCom = std::make_shared<MockPhysicalCommunicator>(LogicalAddress(1, 0)));
     comms.push_back(
-        foreignCom = std::make_shared<MockLocalCommunicator>(LogicalAddress(2, 0)));
+        foreignCom = std::make_shared<MockPhysicalCommunicator>(LogicalAddress(2, 0)));
 
     localMessage = std::make_shared<SpaMessage>(LogicalAddress(1, 1), 1);
     foreignMessage = std::make_shared<SpaMessage>(LogicalAddress(2, 1), 1);
     nonConnectedMessage = std::make_shared<SpaMessage>(LogicalAddress(3, 1), 1);
   }
 
-  std::shared_ptr<MockLocalCommunicator> localCom;
-  std::shared_ptr<MockLocalCommunicator> foreignCom;
+  std::shared_ptr<MockPhysicalCommunicator> localCom;
+  std::shared_ptr<MockPhysicalCommunicator> foreignCom;
   std::vector<SpaCommunicator::Com> comms;
   std::shared_ptr<SpaMessage> localMessage;
   std::shared_ptr<SpaMessage> foreignMessage;
@@ -70,7 +70,7 @@ TEST_F(SpaCommunicatorTest, listen)
 
 class _SpaCommunicator : public SpaCommunicator
 {
-public:
+public: 
   _SpaCommunicator(LogicalAddress currentAddress) : SpaCommunicator(currentAddress) {}
   Com _selectCommunicator(LogicalAddress address, std::vector<Com> const &communicators)
   {
@@ -78,25 +78,21 @@ public:
   }
 };
 
-TEST(SpaCommunicator, selectCommunicator)
+TEST_F(SpaCommunicatorTest, selectCommunicator)
 {
-  LogicalAddress l1(1, 1);
-  LogicalAddress l2(2, 2);
-  LogicalAddress l3(3, 3);
-  PhysicalCommunicator ph_1(l1), ph_2(l2);
-  std::vector<std::shared_ptr<PhysicalCommunicator>> comms;
+  LogicalAddress l1(1, 0); // Local 
+  LogicalAddress l2(2, 0); // Foreign
+  LogicalAddress l3(3, 0); // Nonconnected 
+ 
+  _SpaCommunicator spaCom(l1);
 
-  comms.push_back(std::make_shared<PhysicalCommunicator>(ph_1));
-  comms.push_back(std::make_shared<PhysicalCommunicator>(ph_2));
-
-  _SpaCommunicator com(l1);
-
-  std::shared_ptr<PhysicalCommunicator> selected = com._selectCommunicator(l2, comms);
+  auto selected = spaCom._selectCommunicator(l2, comms);
   EXPECT_TRUE(selected->getSubnetAddress() == l2);
 
-  selected = com._selectCommunicator(l1, comms);
+  selected = spaCom._selectCommunicator(l1, comms);
   EXPECT_TRUE(selected->getSubnetAddress() == l1);
 
-  selected = com._selectCommunicator(l3, comms);
+  selected = spaCom._selectCommunicator(l3, comms);
   EXPECT_TRUE(selected == nullptr);
 }
+

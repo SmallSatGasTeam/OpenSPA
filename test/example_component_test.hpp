6ecb85db5ce2128bc1615301be4e7a421ba333cf
Laurel Bingham <laurelgbingham@gmail.com>
2017-04-11 13:46:26 -0600

    hahahaha

--- test/example_component_test.hpp
+++ test/example_component_test.hpp
@@ -5,6 +5,13 @@ class ExampleComponentTest : public::testing::Test
 public:
   virtual void SetUp()
   {
+      LogicalAddress localAddress(1, 0);
+      std::vector<SpaCommunicator::Com> comms = {
+              std::make_shared<LocalCommunicator>(sock, routingTable, localAddress)};
+      std::shared_ptr<SpaCommunicator> spaCom = std::make_shared<SpaCommunicator>(localAddress, comms);
+
+      ExampleComponent comp(spaCom);
+      comp.appInit();
   }
 
 };
@@ -12,5 +19,12 @@ public:
 TEST_F(ExampleComponentTest, component__send_message)
 {
 
-  EXPECT_TRUE(false);
+ Example_Component example;
+ LogicalAddress address(1, 0);
+ uint8_t opcode = 1;
+ shared_ptr<SpaMessage> mesg = std::make_shared<SpaMessage>(address, opcode); //possibly replace with a 1 instead of using a variable
+ example.sendMsg(mesg);
+
+  EXPECT_TRUE(example.sent);
+
 }
